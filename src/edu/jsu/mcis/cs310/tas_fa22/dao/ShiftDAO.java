f9f86ef9da4b3f23d8ea53784aed9ee8a04e1364
Zach Wolfe <Ztwolfe99@gmail.com>
2022-10-24 12:15:15 -0500

    PunchDAO worked on not finished

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
@@ -9,8 +9,68 @@ public class ShiftDAO {
     
     private final DAOFactory daoFactory;
     
-    ShiftDAO(DAOFactory daofactory) {
+    ShiftDAO(DAOFactory daoFactory){
         
         this.daoFactory = daoFactory;
+        
+    } 
+    
+    public Shift find(String id){
+        
+        Shift shift = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try{
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setString(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        String description = rs.getString("words");
+                        shift = new Shift();
+                    }
+                }
+            }
+        }
+        
+         catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return shift;
     }
 }
+
+ 
