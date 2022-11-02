fdb83680f3f75cb28bd911f051392aec7efc3459
Zach Wolfe <Ztwolfe99@gmail.com>
2022-11-02 11:20:59 -0500

    No errors in shiftDAO

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
@@ -2,10 +2,12 @@ package edu.jsu.mcis.cs310.tas_fa22.dao;
 
 import edu.jsu.mcis.cs310.tas_fa22.*;
 import java.sql.*;
+import java.util.*;
 
 public class ShiftDAO {
     
-    private static final String QUERY_FIND = "words";
+    private static final String QUERY_FIND = "SELECT * FROM shift WHERE id = ?";
+    private static final String QUERY_BADGE = "SELECT * FROM employee WHERE badgeid = ?";
     
     private final DAOFactory daoFactory;
     
@@ -15,7 +17,7 @@ public class ShiftDAO {
         
     } 
     
-    public Shift find(String id){
+    public Shift find(int id){
         
         Shift shift = null;
         
@@ -29,7 +31,7 @@ public class ShiftDAO {
             if (conn.isValid(0)) {
 
                 ps = conn.prepareStatement(QUERY_FIND);
-                ps.setString(1, id);
+                ps.setInt(1, id);
 
                 boolean hasresults = ps.execute();
 
@@ -37,16 +39,22 @@ public class ShiftDAO {
 
                     rs = ps.getResultSet();
 
-                    while (rs.next()) {
-
-                        String description = rs.getString("words");
-                        shift = new Shift();
+                     while (rs.next()) {
+                        
+                        Hashmap<String, String> HashMap = new map<>();
+                        hashmap.put("id", rs.getString("id"));
+                        hashmap.put("description", rs.getString("description"));
+                        hashmap.put("startTime", rs.getString("shiftstart"));
+                        hashmap.put("stopTime", rs.getString("shiftstop"));
+                        hashmap.put("lunchStart", rs.getString("lunchstart"));
+                        hashmap.put("lunchStop", rs.getString("lunchstop"));
+                        shift = new Shift(hashmap);
                     }
                 }
             }
         }
         
-         catch (SQLException e) {
+                  catch (SQLException e) {
 
             throw new DAOException(e.getMessage());
 
@@ -70,7 +78,64 @@ public class ShiftDAO {
         }
 
         return shift;
+
+    }
+    
+    public Shift find(Badge badge) {
+        Shift shift = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_BADGE);
+                ps.setString(1, badge.getId());
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+                    rs.next();
+                    int shiftid = rs.getInt("shiftid");
+                    shift = find(shiftid);
+
+                }
+
+            }
+
+        } 
+        catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } 
+        finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return shift;
     }
 }
 
- 
+   
