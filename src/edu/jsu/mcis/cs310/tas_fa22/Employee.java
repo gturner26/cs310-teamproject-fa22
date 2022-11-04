0139ab26e36dbbd106eb8ac3329808d71980b6cf
Georgia Turner <gturner2602@gmail.com>
2022-11-04 12:08:25 -0500

    Completes Employee and EmployeeDAO (still needs Department for it to work)

--- src/edu/jsu/mcis/cs310/tas_fa22/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Employee.java
@@ -1,5 +1,73 @@
 package edu.jsu.mcis.cs310.tas_fa22;
 
+import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Employee {
+    private String id;
+    private String firstname, middlename, lastname;
+    private Badge badge;
+    private LocalDateTime active;
+    private Department department; //if there is an error it doesn't exist yet
+    private Employee employee;
+    private Shift shift;
+
+    public Employee(HashMap map) {
+        this.id = id;
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.badge = badge;
+        this.active = active;
+        this.department = department;
+        this.employee = employee;
+        this.shift = shift;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+
+    public LocalDateTime getActive() {
+        return active;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+    
+    public Shift getShift(){
+        return shift;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", firstname=" + firstname + ", middlename=" 
+                + middlename + ", lastname=" + lastname + ", badge=" + badge + ", active=" 
+                + active + ", department=" + department + ", employee=" + employee+ ", shift=" 
+                + shift + '}';
+    }
+    
     
 }
--- src/edu/jsu/mcis/cs310/tas_fa22/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/EmployeeDAO.java
@@ -1,5 +1,144 @@
 package edu.jsu.mcis.cs310.tas_fa22.dao;
 
+import edu.jsu.mcis.cs310.tas_fa22.*;
+import java.sql.*;
+import java.util.*;
+
 public class EmployeeDAO {
     
+    private static final String QUERY_FIND = "SELECT * FROM shift WHERE id = ?";
+    private static final String QUERY_BADGE = "SELECT * FROM employee WHERE badgeid = ?";
+    
+    private final DAOFactory daoFactory;
+    
+    EmployeeDAO(DAOFactory daoFactory){
+        
+        this.daoFactory = daoFactory;
+        
+    } 
+    
+    public Employee find(int id){
+        
+        Employee employee = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try{
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                     while (rs.next()) {
+                        
+                        HashMap<String, String> HashMap = new HashMap<>();
+                        HashMap.put("id", rs.getString("id"));
+                        HashMap.put("firstname", rs.getString("firstname"));
+                        HashMap.put("middlename", rs.getString("middlename"));
+                        HashMap.put("lastname", rs.getString("lastname"));
+                        HashMap.put("badge", rs.getString("badge"));
+                        HashMap.put("active", rs.getString("active"));
+                        HashMap.put("department", rs.getString("department"));
+                        HashMap.put("shift", rs.getString("shift"));
+                        HashMap.put("employee", rs.getString("employee"));
+                        employee = new Employee(HashMap);
+                    }
+                }
+            }
+        }
+        
+                  catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return employee;
+
+    }
+    
+    public Employee find(String id) {
+        Employee employee = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_BADGE);
+                ps.setString(1, employee.getId());
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+                    rs.next();
+                    String id = rs.getString("id");
+                    employee = find(id);
+
+                }
+
+            }
+
+        } 
+        catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } 
+        finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return employee;
+    }
 }
+
+   
