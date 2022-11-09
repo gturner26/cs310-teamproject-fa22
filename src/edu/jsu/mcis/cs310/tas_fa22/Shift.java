e86f5c11506c09e1d76c1adc938003b84e7f890e
Austin Ogletree <ogletree.austin@yahoo.com>
2022-11-09 11:23:35 -0600

    Commit

--- src/edu/jsu/mcis/cs310/tas_fa22/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Shift.java
@@ -8,30 +8,39 @@ import java.time.temporal.*;
 public class Shift {
     
     private final int id;
-    //private final int roundinterval, graceperiod, lunchthreshold, dockpenalty;
-    //private final int lunchduration, shiftduration;
+    private final int roundinterval, graceperiod, lunchthreshold, dockpenalty;
     private final String description;
-    private final long lunchtime, shifttime;
     
     private final LocalTime shiftstart, shiftstop, lunchstart, lunchstop;
     
+    private final int lunchduration, shiftduration;
     
     public Shift(HashMap map) {
+        
         this.id = Integer.parseInt((String)map.get("id"));
+        
         this.description = map.get("description").toString();
+        
         this.shiftstart = LocalTime.parse((String)map.get("shiftstart"));
         this.shiftstop = LocalTime.parse((String)map.get("shiftstop"));
         this.lunchstart = LocalTime.parse((String)map.get("lunchstart"));
-        this.lunchstop = LocalTime.parse((String)map.get("lunchStop"));
-        this.lunchtime = Duration.between(lunchstart, lunchstop).toMinutes();
-        this.shifttime = Duration.between(shiftstart, shiftstop).toMinutes();
+        this.lunchstop = LocalTime.parse((String)map.get("lunchstop"));
+        
+        this.lunchduration = (int)Duration.between(lunchstart, lunchstop).toMinutes();
+        this.shiftduration = (int)Duration.between(shiftstart, shiftstop).toMinutes();
+        
+        this.roundinterval = Integer.parseInt((String)map.get("roundinterval"));
+        this.graceperiod = Integer.parseInt((String)map.get("graceperiod"));
+        this.dockpenalty = Integer.parseInt((String)map.get("dockpenalty"));
+        this.lunchthreshold = Integer.parseInt((String)map.get("lunchthreshold"));
+        
     }
 
 
     public int getId() {
         return id;
     }
-/*
+
     public int getRoundinterval() {
         return roundinterval;
     }
@@ -55,7 +64,7 @@ public class Shift {
     public int getShiftduration() {
         return shiftduration;
     }
-*/
+
     public String getDescription() {
         return description;
     }
@@ -82,9 +91,9 @@ public class Shift {
         StringBuilder s = new StringBuilder();
         
         s.append(description).append(": ").append(shiftstart).append(" - ").append(shiftstop);
-        s.append(" (").append(shifttime).append(" minutes);");
+        s.append(" (").append(shiftduration).append(" minutes);");
         s.append(" Lunch: ").append(lunchstart).append(" - ").append(lunchstop);
-        s.append(" (" ).append(lunchtime).append(" minutes)");
+        s.append(" (" ).append(lunchduration).append(" minutes)");
         
         return(s.toString() );
         
