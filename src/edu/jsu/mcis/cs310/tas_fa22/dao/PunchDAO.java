e29db9875e1bcbececc45c585049dde340850130
aehigdon1 <aehigdon1@gmail.com>
2022-11-16 11:24:14 -0600

    Create Method in PunchDAO

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
@@ -8,7 +8,7 @@ public class PunchDAO {
         
  private static final String QUERY_FIND = "SELECT * FROM event WHERE id = ?";
  private static final String QUERY_LIST = "SELECT * FROM event WHERE badgeid = ? ORDER BY timestamp";
- 
+ private static final String QUERY_CREATE = "INSERT INTO event (badgeid, originaltimestamp, terminalid, eventtypeid) VALUES (?,?,?,?)";
 
     private final DAOFactory daoFactory;
     
@@ -84,49 +84,46 @@ public class PunchDAO {
         return punch;
     }
 
-    public Punch create(){
+    public Integer create(Punch p){
         //getBadge()
         //getOriginaltimestamp()
         //getTerminalid()
         //getPunchtype()
         
-        Punch punch = null;
-        
         PreparedStatement ps = null;
         ResultSet rs = null;
+        
+        int key = 0, result = 0;
         
         try{
             
             Connection conn = daoFactory.getConnection();
             
             if (conn.isValid(0)){
-                
-                ps = conn.prepareStatement(QUERY_FIND);
+                ResultSet keys;
+
+                ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
                 
                 boolean hasresults = ps.execute();
                 
                 if (hasresults) {
                     
-                    rs = ps.getResultSet();
                     
-                    while (rs.next()) {
-                        
-                        
-                        String badgeid = rs.getString("badgeid");
-                        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
-                        
-                        Employee id = EmployeeDAO.create(id);
-                        Badge b = badgeDAO.create(badgeid);
-                        EventType desc = EventType.create(desc);
-                        Punch terminalid = Punch.create(terminalid);
-                        punch = new Punch(id, terminalid, b, eventtype);
-                        
+                    ps.setString(1, badgeid);
+                    ps.setString(2, (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(originaltimestamp.getTime()));
+                    ps.setInt(3, 101);
+                    ps.setInt(4, 1);
+                    
+                    result = ps.executeUpdate();
+                    if (result == 1) {
+                        keys = ps.getGeneratedKeys();
+                    if (keys.next()) { key = keys.getInt(1); }
                     }
                 }
             }
             
         }
-        catch (SQLException e) {
+        catch (Exception e) {
             
             throw new DAOException(e.getMessage());
             
